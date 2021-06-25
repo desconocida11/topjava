a2278d81d34c5f83e1c89667835f5d10e3d52f94
desconocida11 <khalitovaae@yandex.ru>
2021-06-25 23:57:44 +0300

    hw5 profiles for repos

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/HsqldbJdbcMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/HsqldbJdbcMealRepository.java
@@ -1,14 +1,17 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Repository;
+import ru.javawebinar.topjava.util.Profiles;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
 @Repository
+@Profile(value = {Profiles.HSQL_DB})
 public class HsqldbJdbcMealRepository extends JdbcMealRepository<Timestamp> {
 
     @Autowired
