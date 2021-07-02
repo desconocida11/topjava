ed70e431f1eb8438a8043f5f8f31c2ab57720d62
desconocida11 <khalitovaae@yandex.ru>
2021-07-03 00:06:57 +0300

    hw6 filter

--- src/main/java/ru/javawebinar/topjava/web/MealController.java
+++ src/main/java/ru/javawebinar/topjava/web/MealController.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +13,14 @@ import ru.javawebinar.topjava.util.MealsUtil;
 import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
 @RequestMapping(value = "/meals")
@@ -24,9 +30,14 @@ public class MealController extends AbstractController {
     private MealService mealService;
 
     @GetMapping
-    public String getMeals(Model model) {
-        int authUserId = SecurityUtil.authUserId();
-        List<MealTo> meals = MealsUtil.getTos(mealService.getAll(authUserId), userService.get(authUserId).getCaloriesPerDay());
+    public String getMeals(HttpServletRequest request, Model model) {
+//        int authUserId = SecurityUtil.authUserId();
+//        List<MealTo> meals = MealsUtil.getTos(mealService.getAll(authUserId), userService.get(authUserId).getCaloriesPerDay());
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+        List<MealTo> meals = getBetween(startDate, startTime, endDate, endTime);
         model.addAttribute("meals", meals);
         return "meals";
     }
@@ -75,5 +86,12 @@ public class MealController extends AbstractController {
     private int getId(HttpServletRequest request) {
         String paramId = request.getParameter("id");
         return paramId.isEmpty() ? 0 : Integer.parseInt(paramId);
+    }
+
+    private List<MealTo> getBetween(@Nullable LocalDate startDate, @Nullable LocalTime startTime,
+                                   @Nullable LocalDate endDate, @Nullable LocalTime endTime) {
+        int userId = SecurityUtil.authUserId();
+        List<Meal> mealsDateFiltered = mealService.getBetweenInclusive(startDate, endDate, userId);
+        return MealsUtil.getFilteredTos(mealsDateFiltered, SecurityUtil.authUserCaloriesPerDay(), startTime, endTime);
     }
 }
